904d78f886cb0aa8ffdb1851a556a036701e9506
beemhuse <brightawah94@gmail.com>
2026-04-28 16:55:19 +0100

    updated email

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://airbnb-free-seven.vercel.app',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:3005',
+      'http://127.0.0.1:3005',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3001;
   await app.listen(port);
